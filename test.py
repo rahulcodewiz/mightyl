6e0215b2f03e3c1665def2cacb27552a36b3fbf4
Rahul Sharma <reethified@gmail.com>
2025-08-04 07:23:38 +0000

    fix contents

---
+++ test.py
@@ -0,0 +1,5 @@
+
+from sjcl import SJCL
+
+op = SJCL().decrypt({"iv":"41R/6wOVIa51oErD31oJpg==","v":1,"iter":1000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"t+v5G1wePvQ=","ct":"wuvwSLbNJoAOsrZZWuToccOYNNoHer6QL1SNw9d8KPgNuGjNeKBci5zFJdWqUzX4axxtP1Hl9G0OHwefKaRatIfI9vz/QTDCMjWvYhMUwYnz0MnUlFvPN2IyCUNQt+C/CqYaoN+sVzO+szgVzF4xCsEzy2dpqZhb8a2DSos63MK3gStIcgz+NTgV8gtcRposMPIOY6NsFCafKXEYs1VythFUuJPM47ZHY0kPFRk5JQ1kIKnQBw+wgP2+iIYZB64+CCJMjC7QeqgtgOQOXzJqMJyeUCU0slJflE0q62V8zEF9Kb0GvElzjC/h0kKKh6VsBJILO/G3X5bknwse8KG9frBXl85881rszAKWClFRJ6jIJE1CZfSha2PwHrbvgZFX7n8qxzmnOVdGPEGukeu/nrng6I8YzBlR2pPheWVQUludVSj1TzzBKii6DUiFNJMrDnx36UyE1QxVIcz1kfZ5"}, 
+                    "XNSLSItali@2468")
